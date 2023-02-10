eb3809735ec67ed9da76701b883dbc4274f28e1d
yanavagad <124992302+yanavagad@users.noreply.github.com>
2023-02-10 15:05:29 +0700

    add cal.

--- print_student_id.py
+++ print_student_id.py
@@ -3,4 +3,5 @@ def print_id(student_id):
     print("Student ID = ", student_id)
 
 if __name__ == "__main__":
-    print_id ("34567")
+    print_id ("6513358")
+    calculate_sum(np.array([1.2.3]))
